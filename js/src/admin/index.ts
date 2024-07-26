3d8ed224471a7024290c96f1756b170216a5587c
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-07-26 20:22:58 +0800

    Update index.ts

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,6 +1,6 @@
 import app from 'flarum/admin/app';
 import { ConfigureWithOAuthPage } from '@fof-oauth';
 
-app.initializers.add('blomstra/oauth-slack', () => {
-  app.extensionData.for('blomstra-oauth-slack').registerPage(ConfigureWithOAuthPage);
+app.initializers.add('ssangyongsports/logto, () => {
+  app.extensionData.for('ssangyongsports-oauth-logto').registerPage(ConfigureWithOAuthPage);
 });
